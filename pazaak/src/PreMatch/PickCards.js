5bbdd6f00535425f4cc65496f4c2b72a4a6d69d6
Calvin <calvin5210@gmail.com>
2022-04-18 13:07:11 -0700

    basic css

--- pazaak/src/PreMatch/PickCards.js
+++ pazaak/src/PreMatch/PickCards.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import PlayerCard from '../Cards/PlayerCard'
+import PlayerCard from '../Cards/Card'
+import styles from './PickCards.module.css'
 
 function PickCards() {
     return (
-        <div className='main'>
+        <div className={styles.main}>
+            <PlayerCard value={1} sign={0}/>
+            <PlayerCard value={9} sign={1}/>
+            <PlayerCard value={1} sign={0}/>
+            <PlayerCard value={9} sign={1}/>
+            <PlayerCard value={1} sign={0}/>
+            <PlayerCard value={9} sign={1}/>
+            <PlayerCard value={1} sign={0}/>
+            <PlayerCard value={9} sign={1}/>
             <PlayerCard value={1} sign={0}/>
             <PlayerCard value={9} sign={1}/>
         </div>
