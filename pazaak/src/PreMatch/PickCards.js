f39c2128ffb7da30594917eeaed3efdcd98e8b1d
Calvin <calvin5210@gmail.com>
2022-04-18 13:46:43 -0700

    main deck hardcoded

--- pazaak/src/PreMatch/PickCards.js
+++ pazaak/src/PreMatch/PickCards.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import PlayerCard from '../Cards/Card'
+import Card from '../Cards/Card'
 import styles from './PickCards.module.css'
 
 function PickCards() {
     return (
         <div className={styles.main}>
-            <PlayerCard value={1} sign={0}/>
-            <PlayerCard value={9} sign={1}/>
-            <PlayerCard value={1} sign={0}/>
-            <PlayerCard value={9} sign={1}/>
-            <PlayerCard value={1} sign={0}/>
-            <PlayerCard value={9} sign={1}/>
-            <PlayerCard value={1} sign={0}/>
-            <PlayerCard value={9} sign={1}/>
-            <PlayerCard value={1} sign={0}/>
-            <PlayerCard value={9} sign={1}/>
+            <Card value={1} sign={1} />
+            <Card value={2} sign={1} />
+            <Card value={3} sign={1} />
+            <Card value={4} sign={1} />
+            <Card value={5} sign={1} />
+            <Card value={6} sign={1} />
+
+            <Card value={1} sign={0} />
+            <Card value={2} sign={0} />
+            <Card value={3} sign={0} />
+            <Card value={4} sign={0} />
+            <Card value={5} sign={0} />
+            <Card value={6} sign={0} />
+
+            <Card value={1} special={{type:'+/-'}}/>
+            <Card value={2} special={{type:'+/-'}}/>
+            <Card value={3} special={{type:'+/-'}}/>
+            <Card value={4} special={{type:'+/-'}}/>
+            <Card value={5} special={{type:'+/-'}}/>
+            <Card value={6} special={{type:'+/-'}}/>
         </div>
     )
 }
