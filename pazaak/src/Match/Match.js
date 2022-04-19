9d00c168ac2cf57cdf637cd31e9bfb2d031308db
Calvin <calvin5210@gmail.com>
2022-04-19 13:37:48 -0700

    added match component

--- pazaak/src/Match/Match.js
+++ pazaak/src/Match/Match.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
+import PlayerHand from './PlayerHand';
+/* 
+    player has three choices
 
-const Match = () => {
-
+    stand:
+        set the final count to the current count
+    play card:
+        player can pick a card from current hand.
+        add or subtract current count by picked card value
+        update current count
+    end turn
+        generate house card
+        add house card value to current count
+        update current count
+        
+*/
+const Match = ( {playerHand }) => {
+    const [hand, setHand] = useState(playerHand)
+    const [count, setCount] = useState(0)
+    return (
+        <PlayerHand hand={hand} setHand={setHand} count={count} setCount={setCount}/>
+    )
 }
 
 export default Match;
