27fb440b80edc484d813683f0c473388077809ed
Calvin <calvin5210@gmail.com>
2022-04-22 13:48:46 -0700

    delete comments

--- pazaak/src/Cards/Card.js
+++ pazaak/src/Cards/Card.js
@@ -1,17 +1,5 @@
 import styles from './Card.module.css';
 
-/* 
-if not a match yet
-    each card will have a event handler to add to sideDeck
-        enclose with div tag? Place event handler in div tag?
-        else move all event handlers to div tags 
-if it is a match
-    no event handlers
-
-    const handleAddToSideDeck = (sign, val, special) => {
-
-*/
-
 function Card({match, value,handleAddToSideDeck, sign, special, isHouse, handlePlayCard, card, botCard}) {
     const signReference = {
         0: '-',
