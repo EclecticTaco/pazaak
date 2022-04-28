fa7e1a13d191fe7245d119ffe94cc8a7c96a7e67
Calvin <calvin5210@gmail.com>
2022-04-28 13:52:45 -0700

    changed var to let

--- pazaak/src/PreMatch/PickCards.js
+++ pazaak/src/PreMatch/PickCards.js
@@ -11,13 +11,13 @@ const PickCards = ({ hand, setHand }) => {
 
 	function getRandom(arr, n) {
 		// src: https://stackoverflow.com/questions/19269545/how-to-get-a-number-of-random-elements-from-an-array
-		var result = new Array(n),
+		let result = new Array(n),
 			len = arr.length,
 			taken = new Array(len);
 		if (n > len)
 			throw new RangeError("getRandom: more elements taken than available");
 		while (n--) {
-			var x = Math.floor(Math.random() * len);
+			let x = Math.floor(Math.random() * len);
 			result[n] = arr[x in taken ? taken[x] : x];
 			taken[x] = --len in taken ? taken[len] : len;
 		}
